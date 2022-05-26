64fa4c0fec7e1baff9dac695433327282293885f
unknown <Crk1712@git>
2022-05-26 18:59:54 +0530

    asset api,conn,main sensor api

--- routes/assets.js
+++ routes/assets.js
@@ -9,9 +9,9 @@ const jwt = require('jsonwebtoken');
 // // 
 
 
-router.post('/addAsset', (req, res) => {
+router.post('/addAssetConfig', (req, res) => {
 
-    const sql = `INSERT INTO asset_tbl(PID, ASSET_NAME, ASSET_TYPE, INDUSTRIAL_TYPE, INDUSTRIAL_DATA_SOURCE, CONNECTION_TYPE, TRACKING_DEVICE, SENSOR, SENSOR_CATEGORY, SENSOR_DATA_TYPE, MAC_ADDRESS, COMPANY_ID, CREATED_BY, CREATED_DATE) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,? , ? )`;
+    const sql = `INSERT INTO asset_config_tbl(PID, ASSET_NAME, ASSET_TYPE, INDUSTRIAL_TYPE, INDUSTRIAL_DATA_SOURCE, CONNECTION_TYPE, TRACKING_DEVICE, SENSOR, SENSOR_CATEGORY, SENSOR_DATA_TYPE, MAC_ADDRESS, COMPANY_ID, CREATED_BY, CREATED_DATE) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,? , ? )`;
     const todo = ['', req.body.ASSET_NAME, req.body.ASSET_TYPE, req.body.INDUSTRIAL_TYPE, req.body.INDUSTRIAL_DATA_SOURCE, req.body.CONNECTION_TYPE, req.body.TRACKING_DEVICE, req.body.SENSOR, req.body.SENSOR_CATEGORY, req.body.SENSOR_DATA_TYPE, req.body.MAC_ADDRESS, req.body.COMPANY_ID, req.body.CREATED_BY, new Date()];
 
     db.query(sql, todo, (err, result) => {
@@ -33,9 +33,9 @@ router.post('/addAsset', (req, res) => {
     })
 })
 
-router.get('/getAllAssets/:COMPANY_ID', (req, res) => {
+router.get('/getAllAssetsConfig/:COMPANY_ID', (req, res) => {
     const COMPANY_ID = req.params.COMPANY_ID;
-    let sql = `SELECT * FROM asset_tbl WHERE COMPANY_ID=${COMPANY_ID}`;
+    let sql = `SELECT * FROM asset_config_tbl WHERE COMPANY_ID=${COMPANY_ID}`;
     db.query(sql, (err, result) => {
         // console.log(result)
         if (err) throw err;
@@ -44,6 +44,44 @@ router.get('/getAllAssets/:COMPANY_ID', (req, res) => {
                 data: result,
                 status: 200
             })
+    })
+})
+// connections starts
+router.post('/addConnection', (req, res) => {
+    let sql;
+    let todo;
+    let errMessage;
+    if (req.body.PID) {
+        // update
+        // console.log('update')
+        sql = 'UPDATE asset_connection_type_tbl SET NAME = ?, MODIFY_BY =?,MODIFY_DATE=? WHERE PID=?';
+        todo = [req.body.NAME, req.body.CREATED_BY, new Date(), req.body.PID];
+        errMessage = 'Record updated,successfully'
+    } else {
+        // console.log('add')
+        // add new
+        sql = `INSERT INTO asset_connection_type_tbl(PID, NAME, CREATED_BY, CREATED_DATE) VALUES (?,?,?,?)`;
+        todo = ['', req.body.NAME, req.body.CREATED_BY, new Date()];
+        errMessage = 'Record added,successfully';
+
+    }
+
+    db.query(sql, todo, (err, result) => {
+        // console.log(result)
+        if (err) {
+            throw err;
+            return res.status(400).send({
+                msg: err
+            });
+        }
+        else {
+            res.send({
+                data: result,
+                status: 200,
+                msg: errMessage
+            })
+
+        }
     })
 })
 router.get('/getAssetConnections', (req, res) => {
@@ -71,7 +109,44 @@ router.get('/getAllSensors', (req, res) => {
             })
     })
 })
+router.post('/addSensor', (req, res) => {
+    let sql;
+    let todo;
+    let errMessage;
+    if (req.body.PID) {
+        // update
+        console.log('update')
+        sql = 'UPDATE sensor_type_tbl SET NAME = ?, MODIFY_BY =?,MODIFY_DATE=? WHERE PID=?';
+        todo = [req.body.NAME, req.body.CREATED_BY, new Date(), req.body.PID];
+        errMessage = 'Record updated,successfully'
+        // sql =   `UPDATE sensor_type_tbl SET NAME = ${ req.body.NAME} WHERE PID = ${ req.body.PID}`;
+    } else {
+        console.log('add')
+        // add new
+        sql = `INSERT INTO sensor_type_tbl(PID, NAME, CREATED_BY, CREATED_DATE) VALUES (?,?,?,?)`;
+        todo = ['', req.body.NAME, req.body.CREATED_BY, new Date()];
+        errMessage = 'Record added,successfully';
 
+    }
+
+    db.query(sql, todo, (err, result) => {
+        // console.log(result)
+        if (err) {
+            throw err;
+            return res.status(400).send({
+                msg: err
+            });
+        }
+        else {
+            res.send({
+                data: result,
+                status: 200,
+                msg: errMessage
+            })
+
+        }
+    })
+})
 router.post('/addSensorSubCatg', (req, res) => {
 
     const sql = `INSERT INTO sensor_subcategory_tbl(PID, SENSOR_TYPE_ID, CATEGORY_NAME, CREATED_BY, CREATED_DATE) VALUES (?,?,?,?,?)`;
@@ -108,5 +183,18 @@ router.get('/getSubCatgSensorByID/:SENSOR_TYPE_ID', (req, res) => {
     })
 })
 
+// assets
+router.get('/getAllAssets', (req, res) => {
+    const COMPANY_ID = req.params.COMPANY_ID;
+    let sql = `SELECT * FROM asset_tbl`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        else
+            res.send({
+                data: result,
+                status: 200
+            })
+    })
+})
 
 module.exports = router;
